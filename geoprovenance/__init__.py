f728be08cff08098c78d50f39f9d10528b045e86
davidyshen <d.y.shen@outlook.com>
2025-04-23 15:35:02 +0200

    Bugfix forgot to rename

--- geoprovenance/__init__.py
+++ geoprovenance/__init__.py
@@ -1,6 +1,6 @@
 from .config import *
 from .metadata import *
-from .load import *
+from .find import *
 from .main import *  # Import main for CLI access
 
 __version__ = "0.1.0"  # Example version
